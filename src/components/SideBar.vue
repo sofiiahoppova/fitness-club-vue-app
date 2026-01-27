958e0fa8984dcf0b9a0322f68c4efe100de5390e
sofiiahoppova <sofiahoppova@gmail.com>
2026-01-27 13:06:41 +0200

    Create basic SideBar component as navigation

---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-navigation-drawer permanent color="primary">
+      <v-list>
+        <v-list-item title="FitSpace" subtitle="Club staff system"></v-list-item>
+        <v-divider></v-divider>
+        <v-list-item title="Dashboard" router to="/"> </v-list-item>
+        <v-list-item title="Schedule" router to="/schedule"> </v-list-item>
+        <v-list-item title="Groups" router to="/groups"> </v-list-item>
+        <v-list-item title="Trainers" router to="/trainers"> </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-container>
+</template>
