02fa514e495ba809d2dc3fba639a9a9c29249e69
sofiiahoppova <sofiahoppova@gmail.com>
2026-01-27 13:03:45 +0200

    Add route paths and components to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+
+import Dashboard from "../pages/Dashboard.vue";
+import Schedule from "../pages/Schedule.vue";
+import Trainers from "../pages/Trainers.vue";
+import Groups from "../pages/Groups.vue";
+
+const routes = [
+  { path: "/", component: Dashboard },
+  { path: "/schedule", component: Schedule },
+  { path: "/trainers", component: Trainers },
+  { path: "/groups", component: Groups },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: routes,
+});
 
-export default router
+export default router;
