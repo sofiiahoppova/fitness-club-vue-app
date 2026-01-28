f244164a2b9b5bdc6359908b9f093283561f550f
sofiiahoppova <sofiahoppova@gmail.com>
2026-01-28 23:22:28 +0200

    Refactor main.js: move vuetify config out

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
+import { vuetify } from "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
 
 const app = createApp(App);
 
